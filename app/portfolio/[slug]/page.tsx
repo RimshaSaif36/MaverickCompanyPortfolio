67446f24139c9f5f8c6171f396adacb72da05e9a
RimshaSaif36 <rimshasaif560@gmail.com>
2026-05-13 11:17:07 +0500

    changes

--- app/portfolio/[slug]/page.tsx
+++ app/portfolio/[slug]/page.tsx
@@ -46,7 +46,7 @@ export default async function ProjectDetailPage({
                             <p className="text-black text-2xl md:text-4xl font-black uppercase tracking-tight">
                                 {project.title}
                             </p>
-                            
+
                             <p className="mt-8 text-gray-500 text-base leading-8 max-w-3xl">
                                 {project.summary}
                             </p>
